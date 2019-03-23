1619e9db06d51d02e641502924fb5e3ad1cac87b
Santiago Cruz Barrenechea <js.cruz@uc.cl>
2019-03-23 10:15:16 -0300

    arreglo help path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/help',    to: 'static_pages#help', as: 'help'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
