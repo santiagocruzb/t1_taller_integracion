5daec7b88144dfee6c2e9b86bb1a3459ebd5f910
Santiago Cruz Barrenechea <js.cruz@uc.cl>
2019-06-11 14:40:07 -0400

    start t3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,11 +4,17 @@ require 'json'
 class StaticPagesController < ApplicationController
   def home
     respuesta = RestClient.get "https://swapi.co/api/films"
-
     respuesta = JSON.parse(respuesta)
-
     @response = respuesta['results'].sort_by { |k| k["episode_id"] }
+    query = 'query { allFilms { edges { node { id producers title episodeID director} } } }'
+    respuesta = RestClient.get "https://swapi-graphql-integracion-t3.herokuapp.com/", {params: {'query' => query}}
 
+    
+    respuesta = JSON.parse(respuesta)
+    # respuesta = respuesta['resu']
+    all_films = respuesta['data']['allFilms']['edges']
+    @response = all_films
+    # puts 
   end
 
   def help
